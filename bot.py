f0958d64ab7343c69f51f01c8e70bd5a5551a4dc
ilyabodo <ilyabodo1@gmail.com>
2020-09-15 12:08:32 -0400

    Added data saving with json and added mention toggle

--- bot.py
+++ bot.py
@@ -6,10 +6,11 @@ import os
 import requests
 import time
 import asyncio
+import json
 load_dotenv()
 
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix = '!')
 
 class Item:
 	def __init__(self, title='', body='', ids=''):
@@ -24,7 +25,41 @@ async def on_ready():
 	reddit = praw.Reddit(client_id=os.getenv('REDDIT_CLIENT_ID'),
                      client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
                      user_agent="my user agent")
+	global data
+	with open('data.json', 'r') as file:
+		data = json.load(file)
 	print("READY")
+
+@client.command()
+async def hello(ctx):
+		if str(ctx.message.author.id) in data:
+			await ctx.send('You have already registered!')
+		else:
+			await init_user(str(ctx.message.author.id))
+			await ctx.send('Thank you for registering with Reddit Bot!')
+
+async def init_user(user_id):
+	data[user_id] = []
+	data[user_id].append({'mention': 'false'})
+	print(data)
+	await save_json()
+
+@client.command()
+async def mention(ctx, mention):
+	print(mention)
+	m = str(mention).lower()
+	if (m != 'false' and m != 'true'):
+		await ctx.send('Please only use !mention True or !mention False')
+		return
+	user_id = str(ctx.message.author.id)	
+	if user_id not in data:
+		await ctx.send('You are not registererd for Reddit Bot. Please use the !hello command!')
+		return
+	else:
+		data[user_id][0]['mention'] = m
+		print (data)
+		temp = ' NOT' if m == 'false' else ''
+		await ctx.send(f'Reddit Bot will{temp} @ mention you about found items.')
 
 @client.command()
 async def test(ctx):
@@ -59,11 +94,12 @@ async def new(ctx, subreddit, l='5'):
 		await ctx.send("Either that subreddit doesn't exist, the number of posts is too high, or you formated the command wrong!")		
 
 @client.command()
-async def lookfor(ctx, length: int = 3, mention: bool = False, subreddit: str = 'discordapp', *, items):
+async def lookfor(ctx, length: int = 3, subreddit: str = 'discordapp', *, items):
 
 	keywords = items.split(',')
 	items_list = list() # List to hold all the Items
 	id_list = list()
+	user_id = str(ctx.message.author.id)
 	for x in range (10):
 		for submission in reddit.subreddit(subreddit).new(limit=5):
 			# More attributes/properties of each post can be gathered here
@@ -80,11 +116,16 @@ async def lookfor(ctx, length: int = 3, mention: bool = False, subreddit: str = 
 							title = (f'{submission.title}'),
 							url = (f'{submission.url}')
 							)
-						if mention == True:
+						if data[user_id][0]['mention'] == 'true':
 							await ctx.send("{} Keyword: '".format(ctx.author.mention) + word + "' was found", embed=e)
 						else:
 							await ctx.send("Keyword: '" + word + "' was found", embed=e)
-		await asyncio.sleep(5)
+		await asyncio.sleep(10)
 	await ctx.send(f'Search for {items} in {subreddit} has ended.')	
+
+
+async def save_json():
+		with open('data.json', 'w') as outfile:
+			json.dump(data, outfile)	
 
 client.run(os.getenv('DISCORD_API_KEY'))
